891ce436ab5977d6cdccb09d9dd5dd49139d42db
Exequiel Bahamonde Cárcamo <12929395+exequielbc@users.noreply.github.com>
2025-11-06 10:45:45 +1100

    Add more UI to check in page

--- app/checkin/page.tsx
+++ app/checkin/page.tsx
@@ -1,9 +1,17 @@
-import { Cross1Icon } from "@radix-ui/react-icons";
-import { Flex, Heading, IconButton, Tooltip } from "@radix-ui/themes";
+import { CameraIcon, Cross1Icon } from "@radix-ui/react-icons";
+import {
+  Button,
+  Card,
+  Flex,
+  Heading,
+  IconButton,
+  Text,
+  Tooltip,
+} from "@radix-ui/themes";
 
 export default function CheckinPage() {
   return (
-    <Flex direction="column">
+    <Flex direction="column" gap="4">
       <Flex justify="between" wrap="wrap">
         <Heading size="6">Check in to a facility</Heading>
         <Tooltip content="Go home">
@@ -12,6 +20,22 @@ export default function CheckinPage() {
           </IconButton>
         </Tooltip>
       </Flex>
+
+      <Card>
+        <Flex direction="column" gap="4">
+          <Flex direction="column" gap="2" align="center">
+            <CameraIcon height="2rem" width="2rem" />
+            <Text size="5">Scan facility QR code</Text>
+            <Text size="4" color="gray">
+              Point your camera at the facility's QR code to check in
+            </Text>
+          </Flex>
+
+          <Button>
+            <CameraIcon /> Start camera
+          </Button>
+        </Flex>
+      </Card>
     </Flex>
   );
 }
